baf514db654d7dce8375630147b6224d948ea5e0
Pasquale <pasqualepiobasile@gmail.com>
2022-09-20 15:07:30 +0200

    Modifica Es2Punto1Server

--- Layer7/Esercitazione1/Esercizio2/Punto 1/TCPServer.py
+++ Layer7/Esercitazione1/Esercizio2/Punto 1/TCPServer.py
@@ -1,17 +1,24 @@
+from contextlib import nullcontext
 from socket import *
+from time import sleep
 
 serverPort = 6789
 
 welcomeSocket = socket(AF_INET,SOCK_STREAM)
 welcomeSocket.bind(('',serverPort)) 
 welcomeSocket.listen(5)  
+connectionSocket, addr = welcomeSocket.accept() 
+clientSentence = "-"
 while 1:
-  
-    connectionSocket, addr = welcomeSocket.accept() 
-    clientSentence = connectionSocket.makefile().readline()
-    if clientSentence == "END":
-        break
-    capitalizedSentence = clientSentence.upper() 
-    connectionSocket.makefile("w").writelines(capitalizedSentence+"\n")
+    if clientSentence == "END" or clientSentence == "":
+        connectionSocket, addr = welcomeSocket.accept()
+        print(" addr: " + str(addr))
+        clientSentence = "-"
+    else:
+        clientSentence = connectionSocket.makefile().readline()
+        print ("Client sentence: " + clientSentence)
+        capitalizedSentence = clientSentence.upper() 
+        connectionSocket.makefile("w").writelines(capitalizedSentence+"\n")
+
 
 connectionSocket.close()
